24a6b18c31bd13cabfa996af9246330d22206679
Juan Sebastian S Paez Paez <jpaezpae@brown-fe03.rcac.purdue.edu>
2021-08-27 05:32:12 -0400

    removed unsupported modifications

--- RUN.bash
+++ RUN.bash
@@ -5,10 +5,10 @@ for i in {1..1} ; do
         -j 200 --cluster-config cluster.json \
         --cluster "sbatch -A {cluster.account} --mem={cluster.mem} -t {cluster.time} --cpus-per-task={cluster.cpus-per-task} -N {cluster.N}" \
         --local-cores 1 \
-        --keep-going \
         -s workflow.smk \
         --config tsv_file=target_files/proteometools_ptm.tsv \
         --rerun-incomplete
+        # --keep-going \
 done
 
 # test_target_files/small_sample_info.tsv
