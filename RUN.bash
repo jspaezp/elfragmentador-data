1a342af8050e72c7100eef3ea582c2c1ec2a36a6
Juan Sebastian S Paez Paez <jpaezpae@brown-fe03.rcac.purdue.edu>
2021-08-27 04:50:57 -0400

    updated run script

--- RUN.bash
+++ RUN.bash
@@ -5,10 +5,14 @@ for i in {1..1} ; do
         -j 200 --cluster-config cluster.json \
         --cluster "sbatch -A {cluster.account} --mem={cluster.mem} -t {cluster.time} --cpus-per-task={cluster.cpus-per-task} -N {cluster.N}" \
         --local-cores 1 \
+        --keep-going \
         -s workflow.smk \
-        --config tsv_file=test_target_files/small_proteometools_ptm.tsv \
+        --config tsv_file=target_files/proteometools_ptm.tsv \
         --rerun-incomplete
 done
 
 # test_target_files/small_sample_info.tsv
-# 
+# test_target_files/small_proteometools_ptm.tsv
+# target_files/proteometools_ptm.tsv
+# target_files/sample_info.tsv
+# target_files/eval_peptidome_sample_info.tsv
--- mokapot_utils.py
+++ mokapot_utils.py
@@ -14,13 +14,17 @@ def filter_mokapot_psm(psm_input: str, peptide_input: str):
 
     df = pd.read_table(str(psm_input))
     pep_df = pd.read_table(str(peptide_input))
+    print(f"Filtering peptides for {str(psm_input)} {str(peptide_input)}")
     print(f"Pep df length {len(pep_df)}")
     pep_df = pep_df[
         (pep_df["mokapot q-value"] < 0.01) & (pep_df["mokapot PEP"] < 0.01)
     ]
     passed_peptides = set(pep_df["Peptide"])
+    print(f"Unique Peptides Passed {len(passed_peptides)}")
+    
+    if len(passed_peptides) == 0:
+        raise ValueError(f"Numbber of peptides that passed is 0 for {psm_input} {peptide_input}")
 
-    print(f"Unique Peptides {len(passed_peptides)}")
     print(f"Number of PSMs {len(df)}")
     df = df[(df["mokapot q-value"] < 0.01) & (df["mokapot PEP"] < 0.01)]
 
