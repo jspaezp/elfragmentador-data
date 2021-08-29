f083311578b739744794da0efa469ecb2907c670
Juan Sebastian S Paez Paez <jpaezpae@brown-fe03.rcac.purdue.edu>
2021-08-29 03:17:19 -0400

    modified train all

--- RUN.bash
+++ RUN.bash
@@ -10,12 +10,13 @@ for i in {1..1} ; do
         -j 800 --cluster-config cluster.json \
         --cluster "sbatch -A {cluster.account} --mem={cluster.mem} -t {cluster.time} --cpus-per-task={cluster.cpus-per-task} -N {cluster.N}" \
         -s workflow.smk \
-        --config tsv_file=target_files/TMT_mod_files.tsv \
+        --config tsv_file=target_files/train_all.tsv \ 
         --rerun-incomplete ${@}
         # --keep-going \
         # --verbose \
 done
 
+#        --config tsv_file=target_files/TMT_mod_files.tsv \
 # test_target_files/small_sample_info.tsv
 # test_target_files/small_proteometools_ptm.tsv
 # target_files/proteometools_ptm.tsv
